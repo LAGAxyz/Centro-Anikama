c73b93e6a8bad13c99708a3e68d9626946645a6f
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2021-08-02 19:59:47 -0500

    todas las secciones editadas OK

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,14 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
 (function() {
   "use strict";
 
   /**
    * Easy selector helper function
    */
+  
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -22,6 +24,7 @@
   /**
    * Easy event listener function
    */
+
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -36,6 +39,7 @@
   /**
    * Easy on scroll event listener 
    */
+
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
@@ -43,6 +47,7 @@
   /**
    * Navbar links active state on scroll
    */
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -63,6 +68,7 @@
   /**
    * Scrolls to an element with header offset
    */
+
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -77,6 +83,7 @@
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
+
   let selectHeader = select('#header')
   let selectTopbar = select('#topbar')
   if (selectHeader) {
@@ -100,6 +107,7 @@
   /**
    * Back to top button
    */
+
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -116,6 +124,7 @@
   /**
    * Mobile nav toggle
    */
+
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
@@ -125,6 +134,7 @@
   /**
    * Mobile nav dropdowns activate
    */
+
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -135,6 +145,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
+
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -153,6 +164,7 @@
   /**
    * Scroll with ofset on page load with hash links in the url
    */
+
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -164,6 +176,7 @@
   /**
    * Preloader
    */
+
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
@@ -174,6 +187,7 @@
   /**
    * Initiate glightbox 
    */
+
   const glightbox = GLightbox({
     selector: '.glightbox'
   });
@@ -181,6 +195,7 @@
   /**
    * Initiate Gallery Lightbox 
    */
+
   const galelryLightbox = GLightbox({
     selector: '.galelry-lightbox'
   });
@@ -188,6 +203,7 @@
   /**
    * Testimonials slider
    */
+
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
